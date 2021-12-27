19a217faaeff756f86f5540d78d35f00c76dd7ba
yurirod <yurirobins@gmail.com>
2021-12-27 12:58:43 -0500

    Fiter strings

--- FunctionalParts/_4Filtering.js
+++ FunctionalParts/_4Filtering.js
@@ -9,3 +9,22 @@ const isEven = (x) => x % 2 === 0;
 const filterEvenNumbers = numbers.filter(isEven);
 console.log('Filtered Event Numbers:');
 console.log(filterEvenNumbers);
+
+console.log(`${'-'.repeat(30)}`);
+
+const words = [
+    'Hello',
+    'World',
+    'Moscow'
+];
+console.log('Words :');
+console.log(words);
+
+const isLongerThanFive = (s) => s.length > 5;
+
+const filteredWords = words.filter(isLongerThanFive);
+
+console.log('Filtered Words Longer than 5 Characters:');
+console.log(filteredWords);
+
+

4fca4b54c11f8a2e542ca9fd47c5873ec42273c1
yurirod <yurirobins@gmail.com>
2021-12-27 12:52:18 -0500

    Filtered Event Numbers

--- FunctionalParts/_4Filtering.js
+++ FunctionalParts/_4Filtering.js
@@ -4,8 +4,8 @@ const numbers = [1, 2, 3, 4, 5];
 console.log('Numbers :');
 console.log(numbers);
 
-const evenNumbers = (x) => x % 2 == 0;
+const isEven = (x) => x % 2 === 0;
 
-const filterEvenNumbers = numbers.filter(evenNumbers);
+const filterEvenNumbers = numbers.filter(isEven);
 console.log('Filtered Event Numbers:');
 console.log(filterEvenNumbers);

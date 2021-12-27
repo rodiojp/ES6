ea1285e31970162e9fc726c75c9445fa71ba93e9
yurirod <yurirobins@gmail.com>
2021-12-27 13:21:49 -0500

    -- fixed Filtered Words Longer than 5 Characters

--- FunctionalParts/_4Filtering.js
+++ FunctionalParts/_4Filtering.js
@@ -13,18 +13,16 @@ console.log(filterEvenNumbers);
 console.log(`${'-'.repeat(30)}`);
 
 const words = [
-    'Hello',
-    'World',
-    'Moscow'
+  'Hello',
+  'World',
+  'Moscow',
 ];
 console.log('Words :');
 console.log(words);
 
-const isLongerThan = x => (s) => s.length > x;
+const isLongerThan = (x) => (s) => s.length > x;
 const minLength = 5;
 const filteredWords = words.filter(isLongerThan(minLength));
 
 console.log(`Filtered Words Longer than ${minLength} Characters:`);
 console.log(filteredWords);
-
-

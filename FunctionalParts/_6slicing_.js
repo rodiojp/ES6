97a0a06006a0336c77a86438d61a1904d8f54e60
yurirod <yurirobins@gmail.com>
2021-12-27 15:53:35 -0500

    some fixes

--- FunctionalParts/_6slicing_.js
+++ FunctionalParts/_6slicing_.js
@@ -4,11 +4,10 @@ const numbers = [0, 1, 2, 3, 4, 5];
 console.log('Numbers :');
 console.log(numbers);
 
-const sliced = numbers.slice(3,5);
+const sliced = numbers.slice(3, 5);
 console.log('Array.Slice(3,5):');
 console.log(sliced);
 
 const copiedReversed = numbers.slice().reverse();
 console.log('Array.Slice().Reverse():');
 console.log(copiedReversed);
-
--- FunctionalParts/_7sorting.js
+++ FunctionalParts/_7sorting.js
@@ -11,9 +11,9 @@ console.log(sorted);
 console.log(`${'-'.repeat(30)} Array.Sort(descending)`);
 
 const descending = (a, b) => {
-    if (a > b) return -1
-    else if (a < b) return 1;
-    return 0
+  if (a > b) return -1;
+  if (a < b) return 1;
+  return 0;
 };
 
 const sorteDesc = numbers.slice().sort(descending);

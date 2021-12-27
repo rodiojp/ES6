8244ee9f75eed39a95335c5f78e0ea27edb07ed6
yurirod <yurirobins@gmail.com>
2021-12-27 16:26:16 -0500

    Reducer Sum of Elements of Array

--- FunctionalParts/_8reducing.js
+++ FunctionalParts/_8reducing.js
@@ -4,11 +4,7 @@ const numbers = [0, 1, 2, 3, 4, 5];
 console.log('Numbers :');
 console.log(numbers);
 
-const sliced = numbers.slice(3,5);
-console.log('Array.Reduce():');
-console.log(sliced);
+const sum = (accumulator, element) => accumulator += element;
 
-const reduced = numbers.reduce();
-console.log('Array.Reduce():');
-console.log(reduced);
-
+const sumOfElements = numbers.reduce(sum);
+console.log(`Array.Reduce(sum): ${sumOfElements}`);

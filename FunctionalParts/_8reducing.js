831e076779b3fd18572d2f6e5d80d7e9a5cb8f84
yurirod <yurirobins@gmail.com>
2021-12-27 16:34:47 -0500

    fixed Reducer

--- FunctionalParts/_8reducing.js
+++ FunctionalParts/_8reducing.js
@@ -1,10 +1,13 @@
 console.log(`${'-'.repeat(30)} Array.Reduce()`);
 
-const numbers = [0, 1, 2, 3, 4, 5];
+const numbers = [1, 2, 3, 4, 5];
 console.log('Numbers :');
 console.log(numbers);
 
-const sum = (accumulator, element) => accumulator += element;
+const sum = (accumulator, element) => {
+  console.log(`{accumulator: ${accumulator}, element: ${element}}`);
+  return accumulator + element;
+};
 
-const sumOfElements = numbers.reduce(sum);
-console.log(`Array.Reduce(sum): ${sumOfElements}`);
+const sumOfElements = numbers.reduce(sum, 0);
+console.log(`Array.Reduce(sum, start: 0): ${sumOfElements}`);
